35b1956366e1244ace5ce7881c49799652953a4c
Jeff Haack <jeff.haack@gmail.com>
2012-11-20 17:05:59 +0400

    should be ready to go!  uses tilecache instead of mod_tile

--- scripts/tilecache-setup.sh
+++ scripts/tilecache-setup.sh
@@ -112,6 +112,7 @@ fi
 echo "Updating apt-get"
 sudo apt-get update
 sudo apt-get -y install libfreetype6-dev libtool autoconf make libboost-all-dev
+IP=$(curl ifconfig.me)
 #git clone git://github.com/ramunasd/mod_tile.git
 #apt-get -y install autoconf make
 
@@ -256,6 +257,9 @@ osm2pgsql --slim -U postgres -d $DB_NAME -S $OSM2PGSQL_STYLESHEET --cache-strate
 ## Setup Minutely Mapnik updates ##
 ###################################
 if [[ $DIFF_UPDATE ]]; then
+	echo "##############################################"
+	echo "Importing Diffs..."
+	echo "##############################################"
 	echo "Diff information will be stored in $DIFF_WORKDIR"
 	echo "Using the file $DATA/$FILE"
 	echo "Using the bounding box $MIN_LON,$MIN_LAT,$MAX_LON,$MAX_LAT"
@@ -273,6 +277,9 @@ fi
 # Setup Cron Job
 # We'll create a script to update the database and then add it to the crontab
 if [[ $DIFF_UPDATE ]]; then
+	echo "##############################################"
+	echo "Setting up cron job to apply diffs..."
+	echo "##############################################"
 	touch $DATA/update_osm_db.sh
 	echo "#!/bin/bash
 # This script will update the $DB_NAME database with OpenStreetMap Data...
@@ -324,6 +331,9 @@ fi
 ########################
 ## Get shapefile data ##
 ########################
+echo "##############################################"
+echo "Getting shapefile data..."
+echo "##############################################"
 sudo apt-get -y install unzip
 cd $HOME
 if [ ! -d shared ]; then
@@ -346,7 +356,25 @@ unzip shoreline_300.zip
 #####################
 ## Setup Tilecache ##
 #####################
-
+echo "##############################################"
+echo "Installing TileCache..."
+echo "##############################################"
+cd /var/www
+wget http://tilecache.org/tilecache-2.11.tar.gz
+tar -zvxf tilecache-*.tar.gz
+rm tilecache-*.tar.gz
+mv tilecache-* tilecache
+echo "
+[osm]
+type=Mapnik
+mapfile=/root/tilescape/project/default/default.xml
+spherical_mercator=true" >> tilecache/tilecache.cfg
+echo "
+<Directory /var/www/tilecache>
+     AddHandler cgi-script .cgi
+     Options +ExecCGI
+</Directory>" >> /etc/apache2/apache2.conf
+/etc/init.d/apache2 restart
 
 
 #############################
@@ -376,8 +404,7 @@ echo ""
 #########################################
 cd $THIS
 cp map.html /var/www/map.html
-sed -i s/"TILE_LOCATION"/"$IP\/$DB_NAME"/ /var/www/map.html
-echo "Go to http://$IP/map.html to see."
+echo "Go to http://$IP/map.html to see your tiles working..."
 
 
 
